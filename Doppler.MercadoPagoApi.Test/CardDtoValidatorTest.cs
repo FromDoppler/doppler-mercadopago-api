ea78cc9ca9af2d3a5e02382a770529a88ccc96fe
Emanuel Hugo Cacheda <ecacheda@makingsense.com>
2022-05-09 16:02:17 -0700

    chore: add arrange, act and assert comments

--- Doppler.MercadoPagoApi.Test/CardDtoValidatorTest.cs
+++ Doppler.MercadoPagoApi.Test/CardDtoValidatorTest.cs
@@ -11,88 +11,143 @@ namespace Doppler.MercadoPagoApi
         [Fact]
         public void Should_not_have_error_when_security_code_has_three_numbers()
         {
+            //Arrange
             var cardDto = new CardDto { SecurityCode = "123" };
+
+            //Act
             var result = _validator.TestValidate(cardDto);
+
+            //Assert
             result.ShouldNotHaveValidationErrorFor(x => x.SecurityCode);
         }
 
         [Fact]
         public void Should_not_have_error_when_security_code_has_four_numbers()
         {
+            //Arrange
             var cardDto = new CardDto { SecurityCode = "1234" };
+
+            //Act
             var result = _validator.TestValidate(cardDto);
+
+            //Assert
             result.ShouldNotHaveValidationErrorFor(x => x.SecurityCode);
         }
 
         [Fact]
         public void Should_have_error_when_security_code_has_five_numbers()
         {
+            //Arrange
             var cardDto = new CardDto { SecurityCode = "12345" };
+
+            //Act
             var result = _validator.TestValidate(cardDto);
+
+            //Assert
             result.ShouldHaveValidationErrorFor(x => x.SecurityCode);
         }
 
         [Fact]
         public void Should_have_error_when_security_code_is_empty()
         {
+            //Arrange
             var cardDto = new CardDto { SecurityCode = "" };
+
+            //Act
             var result = _validator.TestValidate(cardDto);
+
+            //Assert
             result.ShouldHaveValidationErrorFor(x => x.SecurityCode);
         }
 
         [Fact]
         public void Should_have_error_when_card_number_is_empty()
         {
+            //Arrange
             var cardDto = new CardDto { CardNumber = "" };
+
+            //Act
             var result = _validator.TestValidate(cardDto);
+
+            //Assert
             result.ShouldHaveValidationErrorFor(x => x.CardNumber);
         }
 
         [Fact]
         public void Should_have_error_when_card_number_is_invalid()
         {
+            //Arrange
             var cardDto = new CardDto { CardNumber = "4588999" };
+
+            //Act
             var result = _validator.TestValidate(cardDto);
+
+            //Assert
             result.ShouldHaveValidationErrorFor(x => x.CardNumber);
         }
 
         [Fact]
         public void Should_not_have_error_when_card_number_is_valid()
         {
+            //Arrange
             var cardDto = new CardDto { CardNumber = "5031755734530604" };
+
+            //Act
             var result = _validator.TestValidate(cardDto);
+
+            //Assert
             result.ShouldNotHaveValidationErrorFor(x => x.CardNumber);
         }
 
         [Fact]
         public void Should_have_error_when_expiration_month_is_empty()
         {
+            //Arrange
             var cardDto = new CardDto { ExpirationMonth = "" };
+
+            //Act
             var result = _validator.TestValidate(cardDto);
+
+            //Assert
             result.ShouldHaveValidationErrorFor(x => x.ExpirationMonth);
         }
 
         [Fact]
         public void Should_not_have_error_when_expiration_month_is_not_empty()
         {
+            //Arrange
             var cardDto = new CardDto { ExpirationMonth = "10" };
+
+            //Act
             var result = _validator.TestValidate(cardDto);
+
+            //Assert
             result.ShouldNotHaveValidationErrorFor(x => x.ExpirationMonth);
         }
 
         [Fact]
         public void Should_have_error_when_expiration_year_is_empty()
         {
+            //Arrange
             var cardDto = new CardDto { ExpirationYear = "" };
+
+            //Act
             var result = _validator.TestValidate(cardDto);
+
+            //Assert
             result.ShouldHaveValidationErrorFor(x => x.ExpirationYear);
         }
 
         [Fact]
         public void Should_not_have_error_when_expiration_year_is_not_empty()
         {
+            //Arrange
             var cardDto = new CardDto { ExpirationYear = "2026" };
+
+            //Act
             var result = _validator.TestValidate(cardDto);
+
+            //Assert
             result.ShouldNotHaveValidationErrorFor(x => x.ExpirationYear);
         }
     }
--- Doppler.MercadoPagoApi.Test/PaymentRequestDtoValidatorTest.cs
+++ Doppler.MercadoPagoApi.Test/PaymentRequestDtoValidatorTest.cs
@@ -11,96 +11,156 @@ namespace Doppler.MercadoPagoApi
         [Fact]
         public void Should_have_error_when_installments_are_different_than_one()
         {
+            //Arrange
             var paymentRequestDto = new PaymentRequestDto { Installments = 5 };
+
+            //Act
             var result = _validator.TestValidate(paymentRequestDto);
+
+            //Assert
             result.ShouldHaveValidationErrorFor(x => x.Installments);
         }
 
         [Fact]
         public void Should_not_have_error_when_installments_are_one()
         {
+            //Arrange
             var paymentRequestDto = new PaymentRequestDto { Installments = 1 };
+
+            //Act
             var result = _validator.TestValidate(paymentRequestDto);
+
+            //Assert
             result.ShouldNotHaveValidationErrorFor(x => x.Installments);
         }
 
         [Fact]
         public void Should_have_error_when_installments_are_empty()
         {
+            //Arrange
             var paymentRequestDto = new PaymentRequestDto { };
+
+            //Act
             var result = _validator.TestValidate(paymentRequestDto);
+
+            //Assert
             result.ShouldHaveValidationErrorFor(x => x.Installments);
         }
 
         [Fact]
         public void Should_have_error_when_transaction_amount_is_less_than_zero()
         {
+            //Arrange
             var paymentRequestDto = new PaymentRequestDto { TransactionAmount = -1 };
+
+            //Act
             var result = _validator.TestValidate(paymentRequestDto);
+
+            //Assert
             result.ShouldHaveValidationErrorFor(x => x.TransactionAmount);
         }
 
         [Fact]
         public void Should_have_error_when_transaction_amount_is_zero()
         {
+            //Arrange
             var paymentRequestDto = new PaymentRequestDto { TransactionAmount = 0 };
+
+            //Act
             var result = _validator.TestValidate(paymentRequestDto);
+
+            //Assert
             result.ShouldHaveValidationErrorFor(x => x.TransactionAmount);
         }
 
         [Fact]
         public void Should_not_have_error_when_transaction_amount_is_more_than_zero()
         {
+            //Arrange
             var paymentRequestDto = new PaymentRequestDto { TransactionAmount = 10 };
+
+            //Act
             var result = _validator.TestValidate(paymentRequestDto);
+
+            //Assert
             result.ShouldNotHaveValidationErrorFor(x => x.TransactionAmount);
         }
 
         [Fact]
         public void Should_have_error_when_trnsaction_amount_is_empty()
         {
+            //Arrange
             var paymentRequestDto = new PaymentRequestDto { };
+
+            //Act
             var result = _validator.TestValidate(paymentRequestDto);
+
+            //Assert
             result.ShouldHaveValidationErrorFor(x => x.TransactionAmount);
         }
 
         [Fact]
         public void Should_not_have_error_when_payment_method_id_is_visa()
         {
+            //Arrange
             var paymentRequestDto = new PaymentRequestDto { PaymentMethodId = "visa" };
+
+            //Act
             var result = _validator.TestValidate(paymentRequestDto);
+
+            //Assert
             result.ShouldNotHaveValidationErrorFor(x => x.PaymentMethodId);
         }
 
         [Fact]
         public void Should_not_have_error_when_payment_method_id_is_master()
         {
+            //Arrange
             var paymentRequestDto = new PaymentRequestDto { PaymentMethodId = "master" };
+
+            //Act
             var result = _validator.TestValidate(paymentRequestDto);
+
+            //Assert
             result.ShouldNotHaveValidationErrorFor(x => x.PaymentMethodId);
         }
 
         [Fact]
         public void Should_not_have_error_when_payment_method_id_is_amex()
         {
+            //Arrange
             var paymentRequestDto = new PaymentRequestDto { PaymentMethodId = "amex" };
+
+            //Act
             var result = _validator.TestValidate(paymentRequestDto);
+
+            //Assert
             result.ShouldNotHaveValidationErrorFor(x => x.PaymentMethodId);
         }
 
         [Fact]
         public void Should_have_error_when_payment_method_id_is_invalid()
         {
+            //Arrange
             var paymentRequestDto = new PaymentRequestDto { PaymentMethodId = "dopplercard" };
+
+            //Act
             var result = _validator.TestValidate(paymentRequestDto);
+
+            //Assert
             result.ShouldHaveValidationErrorFor(x => x.PaymentMethodId);
         }
 
         [Fact]
         public void Should_have_error_when_payment_method_id_is_empty()
         {
+            //Arrange
             var paymentRequestDto = new PaymentRequestDto { PaymentMethodId = "" };
+
+            //Act
             var result = _validator.TestValidate(paymentRequestDto);
+
+            //Assert
             result.ShouldHaveValidationErrorFor(x => x.PaymentMethodId);
         }
     }
